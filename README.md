2659f7742d6d599d2e80231675f6f421dfb8ef94
Starsky Wong <sununs11@gmail.com>
2016-02-11 01:04:37 +0800

    new paperbox theme first commit

--- README.md
+++ README.md
@@ -1,189 +1,37 @@
-# Landscape plus
+# Paperbox
 
-[![Join the chat at https://gitter.im/xiangming/landscape-plus](https://badges.gitter.im/Join%20Chat.svg)](https://gitter.im/xiangming/landscape-plus?utm_source=badge&utm_medium=badge&utm_campaign=pr-badge&utm_content=badge)
+ [**Demo**](http://sun11.me)
+ 
+基于基于Landscape主题的Landscape-Plus主题由本人修改的主题，主要目的是自己使用，Hexo版本是3.1.1，nodejs 4.2.6
 
-针对中国大陆的hexo用户，优化hexo官方主题landscape。支持hexo 3.x 和 hexo 2.x。[**演示**](http://jasonxiang.com/landscape-plus/)
+### 主题特性
 
-## 主题特色
+- **纸张外观**
+关于文章内容以外的东西只有几张纸和一个纸盒子（为了不那么无聊）
 
-+ **主题配置项优化**，你可以将主题配置项放在站点的`_config.yml`中，避免主题更新造成的冲突。
-+ **移除Google库**，改用cloudflare的CDN，加快页面显示速度。
-+ **新增多语言支持**，支持英文、中文简体和中文繁体。
-+ **新增友情链接模块**，已默认开启，修改方法看下面的[常见问题](#常见问题)。
-+ **新增百度分享模块**，已默认开启。
-+ **新增多说评论模块**，开启方法看下面的[常见问题](#常见问题)。
-+ **新增mathjax模块**，即latex数学公式的支持，默认关闭。（感谢 @Svtter 的[pull request](https://github.com/xiangming/landscape-plus/pull/35)）
-+ **新增IE8支持**。
-+ **外观美化**，美化了部分外观样式。
-+ **使用Monokai代码高亮配色**，最流行、最优雅的代码高亮配色方案。
+- **响应式设计**
+最小宽度为320px，确保了在iPhone 4到5s的320x*分辨率的正常显示
 
-主题还在扩展中，欢迎各种**Pull Request**。
+- **诸多bug修复**
+修复了包括archive分页显示，原主题在分享按钮、mobile nav界面、分页条等的诸多细节bug
 
-## 文档目录
+### 对Landscape-Plus的功能修改
 
-+ [安装](#install)
-+ [启用](#enable)
-+ [配置](#config)
-+ [更新](#update)
-+ [常见问题](#troubleshoots)
-+ [更新日志](#logs)
-+ [网站列表](#sites)
-+ [贡献者们](#contribute)
+- **增加Swiftype站内搜索**
+需在配置文件中设置`swiftype_key`
 
-## <a name='install'>安装</a>
+- **移除百度分享**
+因为不明原因（可能是全局变量？）与Swiftype冲突，先执行的js功能正常，后执行的js会出问题，如百度分享的`更多`按钮点击无效
 
-你可以选择要下载的**release**版本：https://github.com/xiangming/landscape-plus/releases
+- **Landscape原主题分享增强**
+将Pinterest更改为Linkedin，增加四个国内社交网站：微博，人人，QQ空间，微信
 
-或者直接使用最新版：（可能会存在bug，谨慎使用）
+- **移除mathjax模块**
+为了解决mathjax公式显示溢出问题，在主题自带的mathjax修改css没成功，但是使用hexo-math插件修改css成功，所以推荐使用hexo-math插件，主题内已包含修正的CSS样式
 
-```bash
-git clone https://github.com/xiangming/landscape-plus.git themes/landscape-plus
-```
+- **增加文章目录（TOC）显示**
+需在文章中设置`toc: true`，默认不显示编号
 
-## <a name='enable'>启用</a>
+### 浏览器支持
 
-修改hexo的配置文件`_config.yml`，把`theme`的值设置为`landscape-plus`
-```yml
-# Extensions
-## Plugins: http://hexo.io/plugins/
-## Themes: http://hexo.io/themes/
-theme: landscape-plus
-```
-
-## <a name='config'>配置</a>
-
-主题的默认配置文件`landscape-plus\_config.yml`：
-
-```yml
-# Header
-menu:
-  Home: /
-  Archives: /archives
-rss: /atom.xml
-
-# Content
-excerpt_link: Read More
-fancybox: false
-mathjax: false
-
-# Sidebar
-sidebar: right
-widgets:
-- category
-- tag
-- tagcloud
-- archive
-- recent_posts
-- links
-
-# Links
-links:
-  主题作者: http://xiguabaobao.com
-  热前端: http://reqianduan.com
-
-# Miscellaneous
-google_analytics:
-favicon: /favicon.png
-twitter:
-google_plus:
-fb_admins:
-fb_app_id:
-
-# Duoshuo
-duoshuo_shortname:
-
-# Baidu share
-baidushare: true
-```
-
-+ `mathjax` - 是否开启latex数学公式
-+ `links` - 友情链接
-+ `duoshuo_shortname` - 多说评论id
-+ `baidushare` - 是否开启百度分享
-
-**建议！** `mathjax`、`links`、`duoshuo_shortname`、`baidushare`配置项也支持放在站点的`_config.yml`中，并且我们建议你这样做。
-
-## <a name='update'>更新</a>
-
-```bash
-cd themes/landscape-plus
-git pull
-```
-
-**提示** 如果更新发生错误，你可以删除整个主题文件夹，然后重新执行[安装](#install)操作。
-
-## <a name='troubleshoots'>常见问题</a>
-
-**问**：**怎么使用landscape plus主题？**
-> 按照上方的步骤进行`安装`、`启用`。
-
-**问**：如何开启多说评论模块？
-> 在站点的`_config.yml`中，增加`duoshuo_shortname: xxx`配置项，xxx是你的多说id。
-
-**问**：如何关闭百度分享模块？
-> 删掉`themes/landscape-plus\_config.yml`中的`baidushare`配置项。
-
-**问**：如何使用RSS分享功能？
-> 请参考这条[issue](https://github.com/xiangming/landscape-plus/issues/31)进行配置。
-
-**问**：怎么添加友情链接？
-> 在站点的`_config.yml`中，增加`links:`配置项。
-
-**问**：怎么切换语言版本？
-> 在站点的配置文件`_config.yml`，修改`language:`配置项，zh-CN为中文简体，zh-TW为中文繁体，default为英文。
-
-**问**：我喜欢原主题顶部的大图，如何恢复？
-> `themes/landscape-plus/source/css/_partial/header.styl`，取消第33行的注释。
-
-**问**：Landscape plus主题的字体配色太闪眼睛了，我怎么换回原主题的样式？
-> 请参考这条[issue](https://github.com/xiangming/landscape-plus/issues/13)进行配置。
-
-**问**：怎么提建议？
-> 主题还在调整中，欢迎[open issue](https://github.com/xiangming/landscape-plus/issues/new)来提建议，参与讨论。
-
-## <a name='logs'>更新日志</a>
-
-### v1.0.5
-+ 主题配置项优化, refs #17
-+ 百度分享样式调整，refs #45, refs #61
-+ 更新主题说明README.md
-
-### v1.0.4
-+ 增加返回顶部功能
-+ 修改渲染方式，现在默认page布局下仅渲染 .md 文件格式，其他格式一律只做复制。（方便添加静态页面，原本需要在每个文件开头添加 **layout: false**）
-+ 添加**mathjax**的模块开关,不需要的可以自己关闭。
-
-特别感谢来自 @myqianlan 的[pull request](https://github.com/xiangming/landscape-plus/pull/39) 和 @bearpaw 的[pull request](https://github.com/xiangming/landscape-plus/pull/53)。
-
-### v1.0.3
-+ 增加对 **IE8** 的支持
-+ 集成 **mathjax** ，即latex数学公式的支持。（感谢 @Svtter 的[pull request](https://github.com/xiangming/landscape-plus/pull/35)）
-
-### v1.0.2
-+ 修改: 优化Generate速度，refs #13
-
-### v1.0.1
-+ 新增: 百度分享模块
-
-### v1.0.0
-+ 修改：根据国情，去掉Google的库，改用cloudflare的cdn
-+ 新增：语言包
-+ 修改：代码高亮配色修改为`Monokai`
-+ 新增：友情链接
-+ 修改：隐藏顶部大图
-+ 修改：主题配色和部分markdown样式
-+ 新增：多说评论模块
-
-## <a name='contribute'>贡献者们</a>
-
-+ [xiangming](https://github.com/xiangming)
-+ [myqianlan](https://github.com/myqianlan)
-+ [HADB](https://github.com/HADB)
-+ [Svtter](https://github.com/Svtter)
-+ [bearpaw](https://github.com/bearpaw)
-
-主题还在扩展中，欢迎各种**Pull Request**。
-
-## <a name='sites'>网站列表</a>
-
-如果你的网站正在使用**landscape-plus**主题，你可以将网址添加到[wiki的网站列表](https://github.com/xiangming/landscape-plus/wiki)。
+- **IE9+和目前版本的其它浏览器**
--- source/js/script.js
+++ source/js/script.js
@@ -25,41 +25,37 @@
     return false;
   });
   /*toTop end*/
-  // Search
-  var $searchWrap = $('#search-form-wrap'),
-    isSearchAnim = false,
-    searchAnimDuration = 200;
 
-  var startSearchAnim = function(){
-    isSearchAnim = true;
+  /*cubeRotate start*/
+  var isIE = function(){
+	return ("ActiveXObject" in window);
   };
+  if( isIE() ) {
+    $('#contenedor').hide();
+  } else {
+    var cube = $('.cube'),
+        offset = $('#contenedor').offset(),
+        offsetleft = (offset.left + 50),
+        offsettop = (offset.top + 50);
 
-  var stopSearchAnim = function(callback){
-    setTimeout(function(){
-      isSearchAnim = false;
-      callback && callback();
-    }, searchAnimDuration);
-  };
-
-  $('#nav-search-btn').on('click', function(){
-    if (isSearchAnim) return;
-
-    startSearchAnim();
-    $searchWrap.addClass('on');
-    stopSearchAnim(function(){
-      $('.search-form-input').focus();
+    cube.on({
+      mousemove: function(e) {
+        $(this).css('transform','rotateX(' + (e.pageY - offsettop) + 'deg) rotateY(' + (e.pageX - offsetleft) + 'deg)');
+        $(this).addClass('noanimar').removeClass('animar');
+      },
+      mouseout: function() {
+        $(this).css('transform','rotateX(-25deg) rotateY(32deg)');
+        $(this).addClass('animar').removeClass('noanimar');
+      }
     });
-  });
-
-  $('.search-form-input').on('blur', function(){
-    startSearchAnim();
-    $searchWrap.removeClass('on');
-    stopSearchAnim();
-  });
+    // console.log('x=' + offsetleft + ', y=' + offsettop);
+  }
+  /* cubeRotate end*/
 
   // Share
   $('body').on('click', function(){
     $('.article-share-box.on').removeClass('on');
+    $('.qrcode').hide();
   }).on('click', '.article-share-link', function(e){
     e.stopPropagation();
 
@@ -67,59 +63,63 @@
       type = $this.attr('data-share'),
       offset = $this.offset();
 
-    if (type == 'baidu') {
-      var box = $('#article-share-box');
-      shareDataUrl = $this.attr('data-url');
-      shareDataTitle = $this.attr('data-title');
+    var url = $this.attr('data-url'),
+    encodedUrl = encodeURIComponent(url),
+    id = 'article-share-box-' + $this.attr('data-id');
+
+    if ($('#' + id).length){
+      var box = $('#' + id);
 
       if (box.hasClass('on')){
         box.removeClass('on');
         return;
       }
+    } else {
+      var html = [
+        '<div id="' + id + '" class="article-share-box">',
+          '<input class="article-share-input" value="' + url + '">',
+          '<div class="article-share-links">',
+            '<a href="https://twitter.com/intent/tweet?url=' + encodedUrl + '" class="article-share-twitter" target="_blank" title="Twitter"></a>',
+            '<a href="https://www.facebook.com/sharer.php?u=' + encodedUrl + '" class="article-share-facebook" target="_blank" title="Facebook"></a>',
+            '<a href="https://plus.google.com/share?url=' + encodedUrl + '" class="article-share-google" target="_blank" title="Google+"></a>',
+            '<a href="https://www.linkedin.com/shareArticle?url=' + encodedUrl + '" class="article-share-linkedin" target="_blank" title="Linkedin"></a>',
+            '<a href="http://service.weibo.com/share/share.php?url=' + encodedUrl + '" class="article-share-weibo" target="_blank" title="Weibo"></a>',
+            '<a href="http://share.renren.com/share/buttonshare.do?link=' + encodedUrl + '" class="article-share-renren" target="_blank" title="Renren"></a>',
+            '<a href="http://sns.qzone.qq.com/cgi-bin/qzshare/cgi_qzshare_onekey?url=' + encodedUrl + '" class="article-share-qq" target="_blank" title="Qzone"></a>',
+            '<a class="article-share-weixin" target="_blank" title="Weixin"></a>',
+          '</div>',
+        '</div>'
+      ].join('');
 
-      $('.article-share-box.on').hide();
+      var box = $(html);
 
-      box.css({
-        top: offset.top + 25,
-        left: offset.left - 25
-      }).addClass('on');
-    } else{
-      var url = $this.attr('data-url'),
-      encodedUrl = encodeURIComponent(url),
-      id = 'article-share-box-' + $this.attr('data-id');
+      $('body').append(box);
+    }
 
-      if ($('#' + id).length){
-        var box = $('#' + id);
+    box.css({
+      top: offset.top + 25,
+      left: offset.left
+    }).addClass('on');
 
-        if (box.hasClass('on')){
-          box.removeClass('on');
-          return;
-        }
+    $('.article-share-weixin').click(function(){
+      var e_qrcode = $(this).parents('.article-share-box').next('.qrcode');
+      if (e_qrcode.length){
+        e_qrcode.show();
       } else {
-        var html = [
-          '<div id="' + id + '" class="article-share-box">',
-            '<input class="article-share-input" value="' + url + '">',
-            '<div class="article-share-links">',
-              '<a href="https://twitter.com/intent/tweet?url=' + encodedUrl + '" class="article-share-twitter" target="_blank" title="Twitter"></a>',
-              '<a href="https://www.facebook.com/sharer.php?u=' + encodedUrl + '" class="article-share-facebook" target="_blank" title="Facebook"></a>',
-              '<a href="http://pinterest.com/pin/create/button/?url=' + encodedUrl + '" class="article-share-pinterest" target="_blank" title="Pinterest"></a>',
-              '<a href="https://plus.google.com/share?url=' + encodedUrl + '" class="article-share-google" target="_blank" title="Google+"></a>',
-            '</div>',
-          '</div>'
-        ].join('');
-
-        var box = $(html);
-
-        $('body').append(box);
+        $(this).parents('.article-share-box').after('<div class="qrcode"></div>');
+        e_qrcode = $(this).parents('.article-share-box').next('.qrcode');
+        e_qrcode.qrcode({
+          "render": "div",
+          "size": 180,
+          "text": encodeURI(url)
+        });
+        e_qrcode.css({
+          top: offset.top + 20,
+          left: offset.left -150,
+        });
       }
+    });
 
-      $('.article-share-box.on').hide();
-
-      box.css({
-        top: offset.top + 25,
-        left: offset.left
-      }).addClass('on');
-    };
   }).on('click', '.article-share-box', function(e){
     e.stopPropagation();
   }).on('click', '.article-share-box-input', function(){
